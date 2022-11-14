1feae84460890c2e0debbfb9035d8a4ec57ac43f
BORETS2002 <BORETS2002@example.com>
2022-11-14 23:59:51 +0300

    funksiyalar qo'lanildi

--- js/main.js
+++ js/main.js
@@ -1,14 +1,17 @@
 const ElList = document.querySelector(".list")
 const Form = document.querySelector(".form")
 const inputSearch = document.querySelector(".input-search")
+const inputAfter = document.querySelector(".search-after")
+const inputYear = document.querySelector(".choose-year")
+const  elSelect  = document.querySelector(".choose")
+const  elSort  = document.querySelector(".choose-sort")
 
 
 function RenderArray(books ,  ElList) {
   ElList.innerHTML = ""
   const Frag = new DocumentFragment()
-books.forEach(item => {
-  const elTemp = document.querySelector(".templat").content.cloneNode(true)
-  
+  books.forEach(item => {
+    const elTemp = document.querySelector(".templat").content.cloneNode(true)
   elTemp.querySelector(".imgs").src = item.imageLink
 elTemp.querySelector(".title").textContent = item.title
 elTemp.querySelector(".autor").textContent = item.author
@@ -20,40 +23,105 @@ elTemp.querySelector(".citi").textContent = item.country
 
  
   Frag.appendChild(elTemp)
-
+  
 })
 ElList.appendChild(Frag)
 }
 RenderArray(books ,ElList )
 
 
-Form.addEventListener("submit" , evt => {
-  evt.preventDefault()
-  
-const elSearchValue = inputSearch.value
 
-const elREgekt = new RegExp(elSearchValue , "gi")
+// Select
+const geners = []
 
-const FindOption = books.filter(item => {
- return item.title.match(elREgekt)
-
+books.forEach(itm => {
+ 
+      if (! geners.includes(itm.language)) {
+        geners.push(itm.language)
+      }
+    
+      
 })
-console.log(FindOption);
+
+const selectQop = new DocumentFragment()
+geners.forEach(item => {
+  const Option = document.createElement("option");
+Option.value =item;
+Option.textContent = item;
+selectQop.appendChild(Option)
+})
+elSelect.appendChild(selectQop)
+
+// Select end
 
 
-if (  FindOption.length > 0  ) {
-RenderArray(FindOption ,ElList )
-  console.log("aaaa");
-} else{
-  ElList.innerHTML = "not Fount"
+
+function FunkSort (arr,item){
+ console.log(item);
+  if (item == "A-Z"  ) {
+    arr.sort((a,b)=>{
+      if ( a.title < b.title ) {
+        return -1 
+      }
+      else if (a.title > b.title){
+        return 1
+       
+      }
+      
+    })
+  }
+
+  if (item == "Z-A"  ) {
+    arr.sort((a,b)=>{
+      
+      if ( a.title < b.title ) {
+        return 1
+      }
+      else if (a.title > b.title){
+        return -1
+      }
+      
+    })
+    
+  }
+ if(item == "min-max"){
+  arr.sort((a,b) => a.pages - b.pages ) 
+ } 
+ if(item == "max-min"){
+  arr.sort((a,b) => b.pages - a.pages ) 
+ } 
+
 }
 
+
+
+
+Form.addEventListener("submit" , evt => {
+  evt.preventDefault()
+  const afterValue = inputAfter.value
+  // const yearValue = Number(inputYear.value)
+  
+  const elREgekt = new RegExp( inputSearch.value , "gi")
+  const elSelectValue = elSelect.value
+  const selectRegek = new RegExp (elSelectValue , "gi")
+  const FindOption = books.filter(item => {
+    
+    
+    const selectRegek = new RegExp (elSelectValue , "gi")
+    return item.title.match(elREgekt) && (item.author.match(afterValue) || afterValue == "" ) && (item.year >= Number(inputYear.value) && (elSelect.value == "All" || item.language.match(selectRegek) ) )
+
+
+  })
+  
+  
+  
+  if (  FindOption.length > 0  ) {
+    RenderArray(FindOption ,ElList )
+  
+     
+    FunkSort(books , elSort.value )
+  } else{
+    ElList.innerHTML = "not Fount"
+  }
 })
 
-// if(fiterPoke.length > 0){
-//   pokeSort(fiterPoke , elsort.value)
-//   domDrew(fiterPoke , Lists)
-// }
-// else{
-//   Lists.innerHTML = "Not Found"
-// }
